e314b79b5d65c75fa641024398afde22410cd2ff
peterwang0329 <peterwang95267@gmail.com>
2026-06-10 15:01:00 +0800

    fix typescript unused variables

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Personal_Website/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
